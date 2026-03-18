03269fd910b2d1ff484faf5e2bcfdff805735281
aanamarshad <FA19-BSE-084@cuilahore.edu.pk>
2026-03-18 07:56:03 +0500

    changes made ....

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,21 +11,7 @@ export const metadata: Metadata = {
   description: 'Providing top-tier consulting services for the Oil & Gas industry. Expert engineering solutions, project management, and procurement services.',
   generator: 'v0.app',
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: 'https://hebbkx1anhila5yf.public.blob.vercel-storage.com/Loopice-Logo0-2-dlzF7rQaR3Wi1huYcaShy9ysDVAtnB.jpeg',
   },
 }
 
